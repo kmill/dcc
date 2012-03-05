0f7499a406cc5a5b72e7ab1db3722df86ad45e1a
Kyle Miller <kmill@mit.edu>
2012-03-05 00:25:03 -0500

    added some documentation for Unify.hs

--- src/Unify.hs
+++ src/Unify.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies,
   FlexibleInstances, FlexibleContexts #-}
 
+-- | The 'Unify' module provides a somewhat general unification
+-- algorithm for the 'Term' type inside instances of the
+-- 'BindingMonad' class.  The algorithm only unifies tree-like
+-- structures.  It could be extended to arbitrary pointed, ordered
+-- structures, but such generality isn't useful for typechecking,
+-- which was this module's intended purpose.
 module Unify( Term(..)
             , UnificationError(..)
             , UnifierData(..)
@@ -18,36 +24,47 @@ import Control.Applicative
 import Control.Monad.State
 import Control.Monad.Error
 
+-- | Unification variable.
 data UVar = UVar Int
             deriving (Eq, Ord)
 
 instance Show UVar where
     show (UVar i) = "@" ++ show i
 
+-- | Structures which can be unified by 'unify'.
 data Term a = Var UVar
             | Term a [Term a]
               deriving (Eq, Show)
-                       
+
 nullaryTerm :: a -> Term a
 nullaryTerm x = Term x []
 
 type UEnv a = Map.Map Int (Term a)
 
+-- | This class does not need any implementation to define an
+-- instance: the default definitions take advantage of each of the
+-- typeclasses in the signature.  This typeclass defines functions
+-- which make the 'unify' function work for its instances.
 class ( Eq a
       , Monad m
       , Functor m
       , MonadState (UnifierData a) m)
     => BindingMonad a m | m -> a where
   
+    -- | Generates a fresh unification variable that's guaranteed to
+    -- be unique inside this 'BindingMonad'.
     genVar :: m UVar
     genVar = do us <- get
                 put us { unifierVarCounter=unifierVarCounter us + 1 }
                 return $ UVar (unifierVarCounter us)
 
+    -- | Gets the binding associated with a particular unification
+    -- variable.
     getBinding :: UVar -> m (Maybe (Term a))
     getBinding (UVar i) = do env <- unifierEnv <$> get
                              return $ Map.lookup i env
 
+    -- | Binds a term to a unification variable.
     addBinding :: UVar -> Term a -> m ()
     addBinding (UVar i) t
         = do d <- get
@@ -77,12 +94,17 @@ expandTerm (Var (UVar i)) env
         Nothing -> (Var (UVar i))
 expandTerm (Term x xs) env = Term x [expandTerm x' env | x' <- xs]
 
-
+-- | This function performs the "occurs check": whether a variable
+-- occurs in a term to prevent cyclical bindings.
 occursIn :: Eq a => UVar -> Term a -> Bool
 occursIn v (Var v2) | v == v2 = True
                     | otherwise = False
 occursIn v (Term _ xs) = any (occursIn v) xs
 
+-- | The 'unify' function implements the unification algorithm.  It
+-- follows the algorithm in "Correcting a Widespread Error in
+-- Unification Algorithms" by Peter Norvig
+-- (http://norvig.com/unify-bug.pdf).
 unify :: ( BindingMonad a m
          , Applicative m
          , MonadError (UnificationError a) m)
