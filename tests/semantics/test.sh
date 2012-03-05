e4cccac9706f914893640e4c10834092212deb8f
Patrick Hulin <patrick.hulin@gmail.com>
2012-03-04 21:19:37 -0500

    added sentences about tests to writeup and made test.sh prettier

--- tests/semantics/test.sh
+++ tests/semantics/test.sh
@@ -7,20 +7,22 @@ runparser() {
 cd `dirname $0`
 
 fail=0
+temp="`mktemp /tmp/XXXXX`"
 
 for file in ./illegal/*; do
-  if runparser $file; then
+  if runparser $file >$temp; then
     echo "[ ] Illegal file $file incorrectly parsed successfully.";
+	cat $temp
     fail=1
   else
     echo "[+]" $file;
-    echo
   fi
 done
 
 for file in ./legal/*; do
-  if ! runparser $file; then
+  if ! runparser $file >$temp; then
     echo "[ ] Legal file $file failed to parse.";
+	cat $temp
     fail=1
   else
       echo "[+]" $file;
