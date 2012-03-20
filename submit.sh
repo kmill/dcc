25903bee0ac2ca1a440cb9d5a7840dadf8b8ab71
Steven Valdez <dvorak42@mit.edu>
2012-03-20 06:55:25 -0400

    Added helpful scripts

---
+++ submit.sh
@@ -0,0 +1,14 @@
+if [ -z $1]; then
+	echo "Invalid argument (./submit.sh [archivename])"
+	exit 1
+fi
+
+if ./test.sh; then
+	./package.sh $1
+	cp $1.tar.gz /mit/6.035/group/le07/submit/
+	echo "Submission Complete"
+else
+	echo "Error with tests. No submission sent."
+fi
+
+exit 0
