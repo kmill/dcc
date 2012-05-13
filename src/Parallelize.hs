8bc91baa95289a8af57f2099d72a406427cc0f44
Steven D Valdez <dvorak42@mit.edu>
2012-05-13 17:00:21 -0400

    Dataflow Fix, fixed by putrick

--- src/Parallelize.hs
+++ src/Parallelize.hs
@@ -70,7 +70,9 @@ fixIncPred loop blockMap
           processLast inst = inst
           headerLabel = loop_header loop
           headerBlock = fromMaybe (error "couldn't find loop header") $ mapLookup headerLabel blockMap
-          CondBranch _ _ elbl _ = lastInst headerBlock
+          elbl :: Label
+          elbl = case (lastInst headerBlock) of
+            CondBranch _ _ elbl _ -> elbl
 
 lastInst :: Block MidIRInst C C -> MidIRInst O C
 lastInst block = case end of
